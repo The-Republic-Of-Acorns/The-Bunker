03c1c3d546e79504c028bc0f2b8f60607b40e8ca
Connor Ghione <connorghione@gmail.com>
2020-11-18 17:54:11 -0800

    Added code for setting up the wheels, differential drive, and joystick onto the robotmap

--- src/main/java/frc/robot/common/RobotMap.java
+++ src/main/java/frc/robot/common/RobotMap.java
@@ -1,12 +1,18 @@
-package frc.robot;
+xpackage frc.robot;
+
 
 <<<<<<< HEAD
 public class RobotMap{
     /**
      * This is cool.
      */
+    public static DifferentialDrive m_drive;
+    public static WPI_VictorSPX m_leftAft = new WPI_VictorSPX(1);
+    public static WPI_VictorSPX m_leftFron = new WPI_VictorSPX(2);
+    public static WPI_VictorSPX m_rightAft = new WPI_VictorSPX(3);
+    public static WPI_VictorSPX m_rightFront = new WPI_VictorSPX(4);
 
-
+    public static Joystick m_joy = new Joystick(0);
 }
 =======
 <<<<<<< HEAD
